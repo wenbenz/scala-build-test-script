efe00ecc62be2a7c3417b34ca6e9985c277dbffc
Ben Zhao <ben@wenbinzhao.com>
2018-02-04 11:47:16 -0500

    Added test option
    Include test names to only run those tests

--- buildTest.sh
+++ buildTest.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 scalac *.scala;
 if [ $? -eq 0 ]; then
-    for file in ./tests/in/*; do
+    if [ $# -eq 1 ]; then
+        tests=*
+    else
+        tests=${@:2}
+    fi
+    for file in ./tests/in/$tests; do
         echo "Testing: $file"
         cat $file | scala $1 | xxd > testingxxd.temp
         file=${file/.in}
         xxd ${file/in/expected}.out > expectedxxd.temp
         echo 'Discrepancies between in and expected:'
         diff testingxxd.temp expectedxxd.temp
-        rm *.temp
     done
+    rm testingxxd.temp expectedxxd.temp
     rm *.class
 else
     echo Compilation Failed!
